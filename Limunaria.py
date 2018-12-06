7879d9000a1c55834c72d45b14101f4d792b50a7
ramonmartinezFIC <45376970+ramonmartinezFIC@users.noreply.github.com>
2018-12-05 22:48:27 -0600

    Update Limunaria.py

--- Limunaria.py
+++ Limunaria.py
@@ -1,3 +1,6 @@
+#AUTORES:
+#EDUARDO PAULINO BARRETO CAMPOS
+#RAMON TRINIDAD MARTINEZ BRAVO
 #SE IMPORTAN LAS LIBRERIAS NECESARIAS
 import folium
 import pandas as pd
@@ -5,16 +8,34 @@ import pandas as pd
 df=pd.read_csv("Coordenadas.csv")
 #CREAMOS EL MAPA CON LALIBRERIA DE FOLIUM CON LA FUNCION MAP
 map=folium.Map(location=[df["LAT"].mean(),df["LON"].mean()],zoom_start=20)
-#ASIGNAMOS LA EDICION DEL MAPA EN UN GRUPO
-fg=folium.FeatureGroup(name="luminaria")
-#CREAMOS UNA VARIABLE PARA ASIGNAR COLOR SOLO A LOS PUNTOS CON CARACTERISTICAS DIFERENTES
-marcador1 = folium.Marker(
-    location=([df["LAT"].mean(),df["LON"].mean()]),
-    popup=folium.Popup(df["COLOR"], max_width=500),
-    icon=folium.Icon(color="black"))
+#CREAMOS UN BLOQUE DONDE DEFINIREMOS LOS COLORES DE LAS LÁMPARAS DE PENDE A SUS CARACTERÍSTICAS
+def color (elev):
+    minimum=int(min(df['COLOR']))
+    step=int((max(df['COLOR'])-min(df['COLOR']))/3)
+    if elev in range(minimum,minimum+step):
+        col='black'        
+    elif elev in range(minimum+step,minimum+step*2):
+        col='red'    
+    else:
+        col='red'
+    return col
+#NOMBRAMOS LAS CARACTERÍSTICAS DEL GRUPO CON SU RESPECTIVO NOMBRE
+fg=folium.FeatureGroup(name="Localizacion de Luminarias")
 #UTILIZAMOS UN CICLO PARA PODER ASIGNAR UN MARCADOR INDEPENDIENTE POR  CADA FILA DEL ARCHIVO CSV
-for lat,lon,caracteristicas, in zip(df["LAT"],df["LON"],df["CARACTERISTICAS"]):
-    fg.add_child(folium.Marker(location=[lat,lon],popup=(folium.Popup(caracteristicas))))
+for lat,lon,caracteristicas,elev in zip(df["LAT"],df["LON"],df["CARACTERISTICAS"],df["COLOR"]):
+    fg.add_child(folium.Marker(location=[lat,lon],popup=(folium.Popup(caracteristicas)),icon=folium.Icon(color=color(elev),icon_color='black')))
 map.add_child(fg)
 #GUARDAMOS EL MAPA CON EXTENCION HTML
 map.save(outfile="Luminarias.html")
+
+
+
+print 'introduce el divisor de 1000/ '
+d1=input()
+n1=1000
+dd1=float(d1)
+z=n1/d1
+print 'Entero',z
+print 'Resto',(n1/dd1)-z
+
+
